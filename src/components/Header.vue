f3ee45169f38a41da18622aed14e3db37ea1704f
Luziane Freitas <freitas.lu@outlook.com>
2025-08-26 10:48:23 -0300

    feat: create initial home page

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <section>
+    <span> Logo </span>
+
+    <span> Quem somos </span>
+
+    <span> Nosso time </span>
+
+    <span> Benefícios </span>
+
+    <button>Ver vagas</button>
+  </section>
+</template>
